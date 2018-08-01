9738331930fa27a5e2942a80be9580d24f6c8fc9
Manny Lertpatthanakul <jml@alum.wpi.edu>
2018-08-01 15:40:56 -0400

    Change name of output DLL

--- LINQtoCSV/Properties/AssemblyInfo.cs
+++ LINQtoCSV/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LINQtoCSV")]
-[assembly: AssemblyDescription("Popular, easy to use library to read and write CSV and tab delimited files. Supports data fields containing commas and line breaks. Supports different character encodings, separator characters and date and number formats. Robust error handling lets you quickly fix problems in large input files.")]
+[assembly: AssemblyTitle("LINQtoCSVForked")]
+[assembly: AssemblyDescription("Forked version of the LINQtoCSV project on github")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matt Perdeck")]
-[assembly: AssemblyProduct("LINQtoCSV")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("LINQtoCSVForked")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
